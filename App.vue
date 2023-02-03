a7732e1c3c14986fe31c10f1a35fb4921009e652
liyingjing <1147991367@qq.com>
2023-02-03 17:49:42 +0800

    项目

---
+++ App.vue
@@ -0,0 +1,23 @@
+<script>
+export default {
+	onLaunch: function() {
+		setTimeout(() => {
+			uni.setTabBarBadge({
+				index: 1,
+				text: '31'
+			});
+			uni.showTabBarRedDot({
+				index: 3
+			});
+		}, 1000);
+	},
+	onShow: function() {
+	},
+	onHide: function() {
+	}
+};
+</script>
+
+<style>
+/*每个页面公共css */ 
+</style>
---
+++ pages/tabbar/tabbar-2/tabbar-2.vue
@@ -0,0 +1,18 @@
+<template>
+	<view class="content">
+		{{message}}
+	</view>
+</template>
+
+<script setup>
+	import {ref} from'vue'
+	const message=ref('关系户')
+</script>
+
+<style>
+	.content {
+		text-align: center;
+		height: 400upx;
+		margin-top: 200upx;
+	}
+</style>
